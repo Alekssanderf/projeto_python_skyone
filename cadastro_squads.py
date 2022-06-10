385d16ebd8b558f096c04cef1b77820d15136379
Alekssanderf <alekssanderf@gmail.com>
2022-06-10 15:33:22 -0300

    criacao_classes_colaborador_dev

--- cadastro_squads.py
+++ cadastro_squads.py
@@ -1,7 +1,7 @@
 class Pessoa:
-    def __init__(self, nome, tel):
+    def __init__(self, nome, fone):
         self.nome = nome
-        self.tel = tel
+        self.fone = fone
 
 
 class Squad:
@@ -12,3 +12,22 @@ class Squad:
 
     def incluir_techlead(self, techlead):
         self.techlead = techlead
+
+    def incluir_dev(self, dev):
+        self.devs.append(dev)
+
+
+class Colaborador(Pessoa):
+    def __init__(self, nome, fone, squad=None):
+        super().__init__(nome, fone)
+        self.squad = squad
+
+    def incluir_squad(self, squad):
+        self.squad = squad
+
+
+class Dev(Colaborador):
+    def __init__(self, nome, fone, cargo, squad=None):
+        super().__init__(nome, fone, squad)
+        self.cargo = cargo
+
